afc9396203903257ac7dd5351b018278ec9042da
coder29yt <coder29ty@gmail.com>
2024-07-10 15:01:14 +0530

    added changes to day9

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -11,6 +11,7 @@ const Signup = () => {
   const [accountType, setAccountType] = useState("");
 
   const handleSignup = async (e) => {
+    // This is to stop the page from loading 
     e.preventDefault();
     try {
       const res = await axios.post(import.meta.env.VITE_API_URL + "/signup", {
