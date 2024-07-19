e59d8afc4d9b6c7d2ca288fe67e62fbedca9a5bf
coder29yt <coder29ty@gmail.com>
2024-07-19 08:07:20 +0530

    day16 comleted

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -71,4 +71,103 @@ const getMyPosts = async (req, res) => {
   }
 };
 
-module.exports = { createPost, getAllPosts, getMyPosts };
+const deletePost = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const post = await Post.findById(id);
+    if (!post)
+      return res
+        .status(404)
+        .json({ success: false, message: "Post not found " });
+
+    const { authorId } = post;
+    await User.findByIdAndUpdate(authorId, {
+      $pull: { uploads: id },
+    });
+
+    // we will not do this as some of the people had already purchased your asset
+    // await Post.findByIdAndDelete(id);
+
+    return res
+      .status(200)
+      .json({ success: true, message: "Post deleted successfully" });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+const searchPosts = async (req, res) => {
+  const { search } = req.query;
+  try {
+    const posts = await Post.find({ title: { $regex: search, $options: "i" } });
+    if (posts.length == 0)
+      return res.status(404).json({ success: false, message: "No post found" });
+
+    return res.status(200).json({ success: false, data: posts });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+const addToFavourites = async (req, res) => {
+  const { authorId } = req.id;
+  const { postId } = req.params;
+  try {
+    const user = await User.findByIdAndUpdate(authorId, {
+      $push: { favourites: postId },
+    });
+    if (!user)
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+
+    return res
+      .status(200)
+      .json({ success: true, message: "Post added to favourites" });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+const removeFromFavourites = async (req, res) => {
+  const { authorId } = req.id;
+  const { postId } = req.params;
+  try {
+    const user = await User.findByIdAndUpdate(authorId, {
+      $pull: { favourites: postId },
+    });
+    if (!user)
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+
+    return res
+      .status(200)
+      .json({ success: true, message: "Post removed from favourites" });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+const getFavourites = async (req, res) => {
+  const authorId = req.id;
+  try {
+    const { favourites } = await User.findById(authorId).populate("favourites");
+    if (!favourites)
+      return res
+        .status(404)
+        .json({ success: false, message: "No favourites added" });
+    return res.status(200).json({ success: true, data: favourites });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+module.exports = {
+  createPost,
+  getAllPosts,
+  getMyPosts,
+  deletePost,
+  searchPosts,
+  addToFavourites,
+  removeFromFavourites,
+  getFavourites,
+};
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -4,10 +4,24 @@ const {
   createPost,
   getMyPosts,
   getAllPosts,
+  deletePost,
+  searchPosts,
+  addToFavourites,
+  removeFromFavourites,
+  getFavourites,
 } = require("../controllers/postController");
 
 router.post("/post/create", verifyToken, createPost);
 router.get("/post/getAll", getAllPosts);
 router.get("/post/myPosts", verifyToken, getMyPosts);
+router.delete("/post/delete/:id", verifyToken, deletePost);
+router.get("/posts/search", searchPosts);
+router.put("/post/addToFavourites/:postId", verifyToken, addToFavourites);
+router.put(
+  "/post/removeFromFavourites/:postId",
+  verifyToken,
+  removeFromFavourites
+);
+router.get("/posts/favourites", verifyToken, getFavourites);
 
 module.exports = router;
